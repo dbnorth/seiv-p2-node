03073e5cd93e5e17113bd765db7c1ec08bf355c4
David North <david.north@C02X30AGJGH7.local>
2020-09-11 22:44:02 -0500

    Updated services

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -69,20 +69,108 @@ exports.findOne = (req, res) => {
         message: "Error retrieving Tutorial with id=" + id
       });
     });
-
 };
 
+// Add a Course  
+
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.number) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Tutorial
+  const course = {
+    dept: req.body.dept,
+    number: req.body.number,
+    name: req.body.name,
+    description: req.body.description,
+    level: req.body.level,
+    hours: req.body.hours
+  };
+
+  // Save Course in the database
+  Course.create(course)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Course."
+      });
+    });
+};
 // Update a Course by the id in the request
 exports.update = (req, res) => {
+  const id = req.params.id;
 
+  Course.update(req.body, {
+      where: {
+        id: id
+      }
+    })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Course was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Course with id=${id}. Maybe Course was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Course with id=" + id
+      });
+    });
 };
 
 // Delete a Course with the specified id in the request
 exports.delete = (req, res) => {
+  const id = req.params.id;
 
+  Course.destroy({
+      where: {
+        id: id
+      }
+    })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Course was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Course with id=${id}. Maybe Course was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Course with id=" + id
+      });
+    });
 };
 
 // Delete all Courses from the database.
 exports.deleteAll = (req, res) => {
-
+  Course.destroy({
+      where: {},
+      truncate: false
+    })
+    .then(nums => {
+      res.send({
+        message: `${nums} Courses were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while removing all courses."
+      });
+    });
 };
